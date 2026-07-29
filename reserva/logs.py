11f06fffc885a30629f9c78ce487697fcb8fec65
vitor-gonzalez <vihunago@gmail.com>
2026-07-29 18:17:49 -0300

    ediçao de logs.py

--- reserva/logs.py
+++ reserva/logs.py
@@ -1,14 +1,20 @@
 import logging
+import sys
 
-logger = logging.getLogger('hotel')
-
+logger = logging.getLogger("hotel")
 logger.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter(
     "%(asctime)s | %(levelname)s | %(message)s"
 )
 
-arquivo = logging.FileHandler("Hotel.log", encoding="utf-8")
-arquivo.setFormatter(formatter)
+if not logger.handlers:
 
-logger.addHandler(arquivo)
+    arquivo = logging.FileHandler("Hotel.log", encoding="utf-8")
+    arquivo.setFormatter(formatter)
+
+    console = logging.StreamHandler(sys.stdout)
+    console.setFormatter(formatter)
+
+    logger.addHandler(arquivo)
+    logger.addHandler(console)
